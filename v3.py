f98dee8492ca519b3d9df0befde953b3a9b1e065
aigerimdev <kalygulovas@gmail.com>
2024-10-30 21:50:59 -0700

    my first commit

---
+++ v3.py
@@ -0,0 +1,30 @@
+import random
+
+# print("Rock....")
+# print("Paper....")
+# # print("Scissors....")
+
+# # player1 = input("Player 1, make your move: ")
+rand_num == random.randint(0,2)
+if rand_num == 0:
+    computer = 'rock'
+
+# # if player1 == player2:
+#     print("It's a tie!")
+# elif player1 == "rock":
+#    if player2 == "scissors":
+#         print("Player1 wins!")
+#    elif player2 == "paper":
+#         print("Player2 wins!")
+# elif player1 == "paper":
+#     if player2 == "rock":
+#         print("player1 wins!")
+#     elif player2 == "scissors":
+#         print("Player2 wins!")
+# elif player1 == "scissors":
+#     if player2 == "rock":
+#         print("Player2 wins!")
+#     elif player2 == "paper":
+#         print("Player1 wins!")
+# else:
+#     print("something went wrong!")
